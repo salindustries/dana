4df4a74a15af8138718eff27e6edf86afdd11c0a
Sallehuddin Abdul Latif <sallehuddin@gmail.com>
2024-09-18 17:30:21 +0800

    update

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -6,7 +6,7 @@
       aria-hidden="true" id="iconSidenav"></i>
     <a class="navbar-brand m-0 p-4 d-flex align-items-center text-wrap" href="{{ route('analytics') }}">
       <img src="{{ asset('assets') }}/img/infaqsio.png" class="navbar-brand-img h-100" alt="main_logo">
-      <span class="ms-2 font-weight-bold text-white">INFAQ by TradaX</span>
+      <span class="ms-2 font-weight-bold text-white">DANA by TradaX</span>
     </a>
   </div>
   <hr class="horizontal light mt-0 mb-2">
